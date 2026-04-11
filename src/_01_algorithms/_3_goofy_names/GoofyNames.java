4ef1412ea4a3ef854abc4e56c3fcd5f3ba378d22
jia-verma <jiaverma28@gmail.com>
2026-04-11 01:29:19 +0000

    Working on fibonacci. Remember.

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -12,6 +12,44 @@ public class GoofyNames {
 
 		// 1. Ask the user to enter their name
 		//String theUsersNameBeforeItIsGoofiefiedFeFieFoFumIsThatHumanBloodISmellQuestionMarkInCaseYouDidNotNoticeSometimesIGetABitCarriedAway = JOptionPane.showInputDialog("What is your name?");
+		String iAmAStringTheIntegerThatTheUserGivesMeAndITellItWhetherItIsPrimeOrNotIAmTheMostImportantVariableInTheProgramSinceITalkToTheUser = JOptionPane.showInputDialog("Pick a number, and I will tell you a positive personality trait about yourself.");
+		int theRealEmphasisIntegerThatTheUserGivesUsEmphasisAndWeEmphasisWillTellThemEmphasisYouAreSoConceitedButYouDidNotEvenCapitalizeI = integer.ParseInt(iAmAStringTheIntegerThatTheUserGivesMeAndITellItWhetherItIsPrimeOrNotIAmTheMostImportantVariableInTheProgramSinceITalkToTheUser);
+		int iHaveNothingToSayOnThisMatterLaughesGetItExclamationMarkIAmAnAppleProductExclamationMark = 0;
+		int i = 1;
+		while (i<=theRealEmphasisIntegerThatTheUserGivesUsEmphasisAndWeEmphasisWillTellThemEmphasisYouAreSoConceitedButYouDidNotEvenCapitalizeI){
+			if (theRealEmphasisIntegerThatTheUserGivesUsEmphasisAndWeEmphasisWillTellThemEmphasisYouAreSoConceitedButYouDidNotEvenCapitalizeI%i){
+				int iHaveNothingToSayOnThisMatterLaughesGetItExclamationMarkIAmAnAppleProductExclamationMark = iHaveNothingToSayOnThisMatterLaughesGetItExclamationMarkIAmAnAppleProductExclamationMark + 1;
+				int i = i + 1;
+			} else {
+				i = i + 1;
+			}
+		}
+		if (theRealEmphasisIntegerThatTheUserGivesUsEmphasisAndWeEmphasisWillTellThemEmphasisYouAreSoConceitedButYouDidNotEvenCapitalizeI == 0){
+			JOptionPane.showMessageDialog(null, "Your number is negative or zero. You tend to think outside of the box.");
+		} else if (theRealEmphasisIntegerThatTheUserGivesUsEmphasisAndWeEmphasisWillTellThemEmphasisYouAreSoConceitedButYouDidNotEvenCapitalizeI == 1){
+			JOptionPane.showMessageDialog(null, "Your number is one. You are dependable and trustworthy.");
+		} else if (theRealEmphasisIntegerThatTheUserGivesUsEmphasisAndWeEmphasisWillTellThemEmphasisYouAreSoConceitedButYouDidNotEvenCapitalizeI == 2){
+			JOptionPane.showMessageDialog(null, "Your number is prime. You are unique and important to those around you.");
+		} else if (theRealEmphasisIntegerThatTheUserGivesUsEmphasisAndWeEmphasisWillTellThemEmphasisYouAreSoConceitedButYouDidNotEvenCapitalizeI%2){
+			JOptionPane.showMessageDialog(null, "Your number is composite. You are flexible and go with the flow.");
+		} else {
+			JOptionPane.showMessageDialog(null, "Your number is a perfect square. You are sincere and others find you interesting and plesant to talk to.")
+		}
+		int asAnIntegerTheFirstNumberOfTheFibonacciSequencePeriodMyOriginalValueIsZeroNoteToSelfMakeThisLongerLater
+		}
+		
+
+
+
+
+
+
+
+
+
+
+
+
 		String goodbye = "Hello?";
 		return goodbye;
 		String theUsersNameBeforeItIsGoofiefiedFeFieFoFumIsThatHumanBloodISmellQuestionMarkInCaseYouDidNotNoticeSometimesIGetABitCarriedAway = "Jia Verma";
